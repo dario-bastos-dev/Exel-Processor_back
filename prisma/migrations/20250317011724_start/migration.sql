c4e36b52029c3df740a848d3726341b883a4c55a
DARIO BASTOS <dario.bastos.dev@gmail.com>
2025-03-17 11:19:10 -0300

    Delete prisma/migrations directory

--- prisma/migrations/20250317011724_start/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "excel_files" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
